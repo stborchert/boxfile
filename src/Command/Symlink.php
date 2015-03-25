a201608d43535c8c06c0a0a4cb6efdcc5f03e8d2
Johannes Haseitl <johannes@undpaul.de>
2015-03-25 17:10:58 +0100

    Added skeleton for symlink command

---
+++ src/Command/Symlink.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace derhasi\boxfile\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class Symlink extends Command
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this
+          ->setName('symlink')
+          ->setDescription('Ensure symlinks')
+          ->addArgument(
+            'environment',
+            InputArgument::REQUIRED,
+            'Name of the environment to set the symlinks for.'
+          )
+          ->addOption(
+            'boxfile',
+            null,
+            InputOption::VALUE_REQUIRED,
+            'Location of the boxfile holding the configuration.',
+            'Boxfile'
+          )
+          ->addOption(
+            'docroot',
+            null,
+            InputOption::VALUE_REQUIRED,
+            'Path to use as root for the specified configuration',
+            'docroot'
+          )
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $environment = $input->getArgument('environment');
+        $boxfile = $input->getOption('boxfile');
+        $docroot = $input->getOption('docroot');
+
+        $output->writeln(sprintf('Env: %s, Boxfile: %s, Docroot: %s', $environment, $boxfile, $docroot));
+    }
+}
